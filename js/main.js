f4cc0bd16f63cd7eda38c0158613eb08437a931f
Timothy Prestby <tjprestby@gmail.com>
2019-02-06 15:22:22 -0600

    Module 3 Activity Commint FINAL

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
  function Initialize(){
      jQueryAjax();
  };
-    
+//create initialization function
 function jQueryAjax(){
         var mydata;
-
+    //assign empty variable
         $.ajax('data/MegaCities.GeoJSON.txt', {
             dataType: 'json',
             success: function(response){
                 mydata=response;
+                //set what data is to be communicated
                 
                 console.log(mydata);
+                //this will show up since it is within the function
             }
         });
         console.log(mydata);
+        //will show up as undefined since it is not in scope
     }
 
 $(document).ready(Initialize);
